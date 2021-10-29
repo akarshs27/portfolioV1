b2e0c8e1be605d9dc368935c4816f479dfbd481f
Akarsh Srivastava <akarshsrivastava@Akarshs-MacBook-Air.local>
2021-10-29 12:32:19 +0530

    Micro level changes

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,7 +3,7 @@ import "./Button.scss";
 const Button = ({ text, onClick, href }) => {
   return (
     <>
-      <a className="btn-primary" href={href} rel="noreferrer" onClick={onClick}>
+      <a className="btn-primary link" href={href} rel="noreferrer" onClick={onClick}>
         {text}
       </a>
     </>
