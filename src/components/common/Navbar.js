5fa08ceaf3a4e1910abebcf0f21bf4937b79c14f
Akarsh Srivastava <akarshsrivastava@Akarshs-MacBook-Air.local>
2021-11-01 00:07:52 +0530

    Fixed overlay

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,71 +1,94 @@
-import { useEffect, useRef } from "react"
-import logo from "../../images/logo.svg"
-import './Navbar.scss';
+import { useEffect, useRef } from "react";
+import logo from "../../images/logo.svg";
+import "./Navbar.scss";
 import { links } from "../../data/constants";
+import { gsap, Linear } from "gsap";
 
-const Navbar = ({timeline, isDesktop}) => {
-    const hamEl = useRef(null);
-    const overlayEl = useRef(null);
-    const linksEl = useRef(null);
-    const logoEl = useRef(null);
+const Navbar = () => {
+  const hamEl = useRef(null);
+  const overlayEl = useRef(null);
+  const linksEl = useRef(null);
+  const logoEl = useRef(null);
+  const targetSection = useRef(null);
+  const overlaySectionEl = useRef(null);
 
-    const toggleHum = () => {
-        hamEl.current.classList.toggle('active');
-        overlayEl.current.classList.toggle('active');
-        linksEl.current.classList.toggle('active');
+  const toggleHum = () => {
+    hamEl.current.classList.toggle("active");
+    if (overlaySectionEl.current.classList.contains("overlay-index")) {
+      overlaySectionEl.current.classList.remove("overlay-index");
+    } else {
+      overlaySectionEl.current.classList.add("overlay-index");
     }
+    overlayEl.current.classList.toggle("active");
+    linksEl.current.classList.toggle("active");
+  };
 
-    const onLinkClicked = (e) => {
-        e.preventDefault();
-        toggleHum();
-        const target = e.target.getAttribute('href');
-        const location = document.querySelector(target).offsetTop;
-        window.scrollTo({
-            left: 0,
-            top: location - 80,
-            behavior: "smooth"
-        })
-    }
+  const onLinkClicked = (e) => {
+    e.preventDefault();
+    hamEl.current.classList.toggle("active");
+    overlaySectionEl.current.classList.remove("overlay-index");
+    overlayEl.current.classList.toggle("active");
+    linksEl.current.classList.toggle("active");
+    const target = e.target.getAttribute("href");
+    const location = document.querySelector(target).offsetTop;
+    window.scrollTo({
+      left: 0,
+      top: location - 80,
+      behavior: "smooth",
+    });
+  };
 
-    useEffect(() => {
-        if(isDesktop) {
-            timeline.from(logoEl.current, 0.5, {
-                opacity: 0,
-                y: 100
-            });
-            timeline.from(hamEl.current, 0.5, {
-                opacity: 0,
-                y: -50,
-                ease: 'ease'
-            });
-        }
-    }, [isDesktop, timeline])
+  useEffect(() => {
+    const revealTl = gsap.timeline({ defaults: { ease: Linear.easeNone } });
+    revealTl
+      .to(targetSection.current, { opacity: 1, duration: 1 })
+      .from(
+        targetSection.current.querySelectorAll(".animItem"),
+        { opacity: 0, duration: 1, stagger: 0.5 },
+        "<"
+      );
+  }, [targetSection]);
 
-    return (
-        <section>
-            <div className="navbar w-full fixed top-0 mx-auto flex px-8  md:px-24  py-2 md:py-4 md:flex-row  items-center justify-between">
-                <a href="/#home" ref={logoEl} className="link cursor-none">
-                    <img className ="h-6" src={logo} alt="akarsh-logo" />
+  return (
+    <section>
+      <div ref={targetSection}>
+        <div className="navbar w-full fixed top-0 mx-auto flex px-8 md:px-24 2xl:px-44 py-2 md:py-4 md:flex-row  items-center justify-between">
+          <a href="/#home" ref={logoEl} className="link cursor-none animItem">
+            <img className="h-6" src={logo} alt="akarsh-logo" />
+          </a>
+          <div
+            ref={hamEl}
+            className="relative w-12 h-12 border border-transparent outer link cursor-none animItem"
+            onClick={toggleHum}
+          >
+            <span className=""></span>
+            <span className=""></span>
+          </div>
+        </div>
+      </div>
+
+      <div className="overlay-section" ref={overlaySectionEl}>
+        <div className="overlay" ref={overlayEl}></div>
+
+        <div className="links flex flex-col space-y-6" ref={linksEl}>
+          {links.map((link) => {
+            return (
+              <div className="link">
+                <a
+                  href={link.url}
+                  key={link.id}
+                  className="text-2xl tracking-wide font-semibold md:text-3xl"
+                  onClick={onLinkClicked}
+                >
+                  {link.text}
                 </a>
-              
-                <div ref={hamEl} className="relative w-12 h-12 border border-transparent outer link cursor-noneˀ" onClick= {toggleHum}>
-                    <span className="" ></span>
-                    <span className="" ></span>
-                </div>
-            </div>
-            <div className="overlay" ref={overlayEl}></div>
-            <div className="links flex flex-col space-y-6" ref={linksEl}> 
-                
-                    { links.map((link) => {
-                        return (
-                            <div className="link">
-                            <a href={link.url} key={link.id} className="text-2xl tracking-wide font-semibold md:text-3xl" onClick={onLinkClicked}>{link.text}</a>
-                            </div>
-                        )
-                    })}
-            </div>
-        </section>
-    )
-}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,29 +1,29 @@
-export const links = [ 
-    {
-        id: '1',
-        text: 'Home',
-        url: '#home'
-    },
-    {
-        id: '2',
-        text: 'Projects',
-        url: '#projects'
-    },
-    {
-        id: '3',
-        text: 'Skills',
-        url: '#skills'
-    },
-    {
-        id: '4',
-        text: 'About',
-        url: '#about'
-    },
-    {
-        id: '5',
-        text: 'Contacts',
-        url: '#contacts'
-    },
+export const links = [
+  {
+    id: "1",
+    text: "Home",
+    url: "#home",
+  },
+  {
+    id: "2",
+    text: "Projects",
+    url: "#projects",
+  },
+  {
+    id: "3",
+    text: "Skills",
+    url: "#skills",
+  },
+  {
+    id: "4",
+    text: "About",
+    url: "#about",
+  },
+  {
+    id: "5",
+    text: "Contacts",
+    url: "#contacts",
+  },
 ];
 
-export const EMAIL = 'akarshs27@gmail.com';
+export const EMAIL = "akarshs27@gmail.com";
